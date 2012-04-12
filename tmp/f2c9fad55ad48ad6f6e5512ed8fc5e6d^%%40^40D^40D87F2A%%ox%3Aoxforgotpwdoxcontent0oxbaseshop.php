d1cdcd8a063fc1801a53036372aa968c5a081133
tomas <tomas@tomas-ubuntu.(none)>
2012-04-12 11:29:04 +0300

    Initial import

---
+++ tmp/f2c9fad55ad48ad6f6e5512ed8fc5e6d^%%40^40D^40D87F2A%%ox%3Aoxforgotpwdoxcontent0oxbaseshop.php
@@ -0,0 +1,8 @@
+<?php /* Smarty version 2.6.26, created on 2012-04-12 10:01:45
+         compiled from ox:oxforgotpwdoxcontent0oxbaseshop */ ?>
+<p>If you don't get an e-mail with your access data, please make sure that you have already registered with us. As soon as you are registered, you can login with your e-mail address and your password.</p>
+<ul>
+<li>
+If you are sure you are already registered, please check the e-mail address you entered as user name.</li></ul>
+<p>
+In case you still have problems logging in, please turn to us by e-mail: <a href="mailto:demo@oxid-esales.com?subject=Password"><strong>demo@oxid-esales.com</strong></a></p>
